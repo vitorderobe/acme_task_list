fa1e27c2e605c0b63692e0189b16cb17b1a74eb8
Vitor Derobe <vitorderobe@gmail.com>
2017-03-26 20:10:09 +0000

    Add Favorite model and references it to List and User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :lists, dependent: :destroy       
+  has_many :lists, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_lists, through: :favorites, source: :list
 end
