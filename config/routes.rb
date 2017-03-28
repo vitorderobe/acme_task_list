ea477e2a5ef118c06385ed5170fdebafc5b726a1
Vitor Derobe <vitorderobe@gmail.com>
2017-03-28 10:19:41 +0000

    Add show list view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root :to =>'home#index'
   devise_for :users
 
+  get  'lists/show'   => 'lists#show'
   post 'lists/create' => 'lists#create'
   post 'tasks/create' => 'tasks#create'
 end
